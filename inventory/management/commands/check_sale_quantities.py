83d0c08e621e670c9a7c89e9e1a7c74056c23533
Kelvin Muza <k1muza@gmail.com>
2025-01-06 08:57:00 +0200

    update check_sale_quantities

--- inventory/management/commands/check_sale_quantities.py
+++ inventory/management/commands/check_sale_quantities.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db import models
 
 from inventory.models import SaleItem
 
@@ -6,10 +7,14 @@ class Command(BaseCommand):
     help = 'Check and resolve sale quantities'
 
     def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS(f'Checking {SaleItem.objects.count()} sale quantities'))
+        count = 0
         for item in SaleItem.objects.all():
-            movement_qnty = sum([m.quantity for m in item.movements.all()])
-            if movement_qnty != item.quantity:
+            movement_total_qnty = item.movements.aggregate(total_qnty=models.Sum('quantity'))['total_qnty']
+            if movement_total_qnty != item.quantity and item.quantity > 0:
                 self.stdout.write(
-                    f"{item.sale.date}, {item.product}, {item.quantity}, {movement_qnty}"
+                    f"{item.sale.date}, {item.product}, {item.quantity}, {movement_total_qnty}"
                 )
+                count += 1
+        self.stdout.write(self.style.SUCCESS(f'Found {count} discrepancies'))
         self.stdout.write(self.style.SUCCESS('Successfully checked sale quantities'))
