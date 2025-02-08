8b27e3201439bcde5b1450b6282b691506be5449
Kelvin Muza <k1muza@gmail.com>
2025-02-08 21:11:21 +0200

    product created at

--- inventory/models/product.py
+++ inventory/models/product.py
@@ -22,6 +22,9 @@ class Product(models.Model):
     predict_demand = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f"{self.name}"
 
